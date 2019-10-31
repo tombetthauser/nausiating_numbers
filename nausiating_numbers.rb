1fde35efde7a062c0d3b0f0a5bfae39fe7c56f76
tombetthauser <tombetthauser@gmail.com>
2019-10-31 15:31:12 -0700

    Set up and tested matrix_addition method

--- nausiating_numbers.rb
+++ nausiating_numbers.rb
@@ -222,16 +222,26 @@ end
 
 # Examples
 
-p anti_prime?(24)   # true
-p anti_prime?(36)   # true
-p anti_prime?(48)   # true
-p anti_prime?(360)  # true
-p anti_prime?(1260) # true
-p anti_prime?(27)   # false
-p anti_prime?(5)    # false
-p anti_prime?(100)  # false
-p anti_prime?(136)  # false
-p anti_prime?(1024) # false
+# p anti_prime?(24)   # true
+# p anti_prime?(36)   # true
+# p anti_prime?(48)   # true
+# p anti_prime?(360)  # true
+# p anti_prime?(1260) # true
+# p anti_prime?(27)   # false
+# p anti_prime?(5)    # false
+# p anti_prime?(100)  # false
+# p anti_prime?(136)  # false
+# p anti_prime?(1024) # false
+
+
+
+
+
+
+
+
+
+
 
 # matrix_addition
 
@@ -248,18 +258,22 @@ p anti_prime?(1024) # false
 # 2 5  +  9 1  =>  11 6
 # 4 7     3 0      7 7
 
+def matrix_addition(matrix_1, matrix_2)
+  "#{matrix_1} // #{matrix_2}"
+end
+
 # Examples
 
-# matrix_a = [[2,5], [4,7]]
-# matrix_b = [[9,1], [3,0]]
-# matrix_c = [[-1,0], [0,-1]]
-# matrix_d = [[2, -5], [7, 10], [0, 1]]
-# matrix_e = [[0 , 0], [12, 4], [6,  3]]
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
 
-# p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
-# p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
-# p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
-# p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
+p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
 
 # mutual_factors
 
