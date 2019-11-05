5b0619b0a95ac98e175037c8660cf5c45de4aae1
tombetthauser <tombetthauser@gmail.com>
2019-11-04 19:14:02 -0800

    Added responses from pair programming with Michael

--- nausiating_numbers.rb
+++ nausiating_numbers.rb
@@ -677,3 +677,525 @@ end
 # p pretentious_primes([32, 5, 11], -3)         # [23, nil, 3]
 # p pretentious_primes([32, 5, 11], -4)         # [19, nil, 2]
 # p pretentious_primes([32, 5, 11], -5)         # [17, nil, nil]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+require "byebug"
+
+def strange_sums(nums)
+    zero_sums = 0
+    
+    nums.each_with_index do |num, i|
+        if i == (nums.length - 1)
+            return zero_sums
+        else
+            (i+1..nums.length-1).each_with_index do |num_2, i_2|
+                if num + nums[num_2] == 0
+                    zero_sums += 1
+                end
+            end
+        end
+    end
+    zero_sums
+end
+
+def strange_sums(nums_arr)
+    (0...nums_arr.length).each do |idx_1|
+        (idx_1+1...nums_arr.length).each do |idx_2|
+            
+        end
+    end
+end
+
+# def strange_sums(nums)
+#     nums.inject([0, 0]) do |acc, ele|
+#         debugger
+#         if (acc[1] + 1 < nums.length) && (ele + nums[acc[1] + 1]) == 0
+#             [acc[0] += 1, (acc[1] += 1)]
+#         else 
+#             [acc[0], (acc[1] += 1)]
+#         end
+#     end[0]
+# end
+
+
+#  p strange_sums([2, -3, 3, 4, -2])     # 2
+#  p strange_sums([42, 3, -1, -42])      # 1
+#  p strange_sums([-5, 5])               # 1
+#  p strange_sums([19, 6, -3, -20])      # 0
+#  p strange_sums([9])                   # 0
+
+def pair_product(nums_array, pro)
+    (0...nums_array.length).each do |idx_1|
+        (idx_1+1...nums_array.length).each do |idx_2|
+            if nums_array[idx_1] * nums_array[idx_2] == pro
+                return true
+            end
+        end
+    end
+    false
+end
+
+# p pair_product([4, 2, 5, 8], 16)    # true
+# p pair_product([8, 1, 9, 3], 8)     # true
+# p pair_product([3, 4], 12)          # true
+# p pair_product([3, 4, 6, 2, 5], 12) # true
+# p pair_product([4, 2, 5, 7], 16)    # false
+# p pair_product([8, 4, 9, 3], 8)     # false
+# p pair_product([3], 12)             # false
+
+
+def rampant_repeats(str, hash)
+    mod_str = ""
+
+    str.each_char do |char|
+        if hash.has_key?(char)
+            hash[char].times {mod_str += char}
+        else
+            mod_str += char
+        end
+    end
+    mod_str
+end
+
+# p rampant_repeats('taco', {'a'=>3, 'c'=>2})               # 'taaacco'
+# p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3})   # 'ffffeeveerisssh'
+# p rampant_repeats('misispi', {'s'=>2, 'p'=>2})            # 'mississppi'
+# p rampant_repeats('faarm', {'e'=>3, 'a'=>2})              # 'faaaarm'
+
+def perfect_square(num)
+    (1..num).each { |ele| return true if ele * ele == num }
+    false
+end
+
+# p perfect_square(1)     # true
+# p perfect_square(4)     # true
+# p perfect_square(64)    # true
+# p perfect_square(100)   # true
+# p perfect_square(169)   # true
+# p perfect_square(2)     # false
+# p perfect_square(40)    # false
+# p perfect_square(32)    # false
+# p perfect_square(50)    # false
+
+def fact_count(num)
+    count = 0
+
+    (1..num).each do |ele|
+        count += 1 if num % ele == 0
+    end
+    count
+end
+
+def anti_prime?(num)
+    num_facts = fact_count(num)
+
+    (1...num).each do |ele|
+        return false if fact_count(ele) > num_facts
+    end
+    true
+end
+
+# p anti_prime?(24)     # true
+# p anti_prime?(36)     # true
+# p anti_prime?(48)     # true
+# p anti_prime?(360)    # true
+# p anti_prime?(1260)   # true
+# p anti_prime?(27)     # false
+# p anti_prime?(5)      # false
+# p anti_prime?(100)    # false
+# p anti_prime?(136)    # false
+# p anti_prime?(1024)   # false
+
+
+def matrix_addition(mat_1, mat_2)
+    mat_sum = []
+    (0...mat_1.length).each do |sub_array_index|
+        temp_sub = []
+        mat_1[sub_array_index].each_with_index do |ele, num_index|
+            temp_sub << (mat_1[sub_array_index][num_index] + mat_2[sub_array_index][num_index])
+        end
+        mat_sum << temp_sub
+    end
+    mat_sum
+end
+
+# matrix_a = [[2,5], [4,7]]
+# matrix_b = [[9,1], [3,0]]
+# matrix_c = [[-1,0], [0,-1]]
+# matrix_d = [[2, -5], [7, 10], [0, 1]]
+# matrix_e = [[0 , 0], [12, 4], [6,  3]]
+# p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+# p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+# p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+# p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
+
+
+def mutual_factors(*numbers)
+    all_factors = Hash.new(0)
+    numbers.each do |number|
+        factor_array(number).each do |factor|
+            all_factors[factor] += 1 
+        end
+    end
+    all_factors.select { |num, times| times == numbers.length }.keys
+end
+
+def factor_array(number)
+    all_factors = []
+    (1..number).each { |ele| all_factors << ele if number % ele == 0 }
+    all_factors
+end
+
+# p mutual_factors(8, 4)              # [ 1, 2, 4]
+# p mutual_factors(8, 4, 10)          # [1, 2]
+# p mutual_factors(12, 24)            # [1, 2, 3, 4, 6, 12]
+# p mutual_factors(12, 24, 64)        # [1, 2, 4]
+# p mutual_factors(50, 30)            # [1, 2, 5, 10]
+# p mutual_factors(50, 30, 45, 105)   # [1, 5]
+# p mutual_factors(22, 44)            # [1, 2, 11, 22]
+# p mutual_factors(22, 44, 11)        # [1, 11]
+# p mutual_factors(7)                 # [1, 7]
+# p mutual_factors(7, 9)              # [1]
+
+
+def tribonacci_number(n)
+    trib = [1,1,2]
+    while trib.length < n
+        trib << trib[-1] + trib[-2] + trib[-3]
+    end
+    trib[n-1]
+end
+
+# p tribonacci_number(1)  # 1
+# p tribonacci_number(2)  # 1
+# p tribonacci_number(3)  # 2
+# p tribonacci_number(4)  # 4
+# p tribonacci_number(5)  # 7
+# p tribonacci_number(6)  # 13
+# p tribonacci_number(7)  # 24
+# p tribonacci_number(11) # 274
+
+
+def matrix_addition_reloaded(*matrices)
+    return nil unless matrices.all? {|mat| mat.length == matrices[0].length}
+    matrices.inject { |sum_mat, mat| matrix_addition(sum_mat, mat)}
+end
+
+# matrix_a = [[2,5], [4,7]]
+# matrix_b = [[9,1], [3,0]]
+# matrix_c = [[-1,0], [0,-1]]
+# matrix_d = [[2, -5], [7, 10], [0, 1]]
+# matrix_e = [[0 , 0], [12, 4], [6,  3]]
+# p matrix_addition_reloaded(matrix_a, matrix_b)              # [[11, 6], [7, 7]]
+# p matrix_addition_reloaded(matrix_a, matrix_b, matrix_c)    # [[10, 6], [7, 6]]
+# p matrix_addition_reloaded(matrix_e)                        # [[0, 0], [12, 4], [6, 3]]
+# p matrix_addition_reloaded(matrix_d, matrix_e)              # [[2, -5], [19, 14], [6, 4]]
+# p matrix_addition_reloaded(matrix_a, matrix_b, matrix_e)    # nil
+# p matrix_addition_reloaded(matrix_d, matrix_e, matrix_c)    # nil
+
+
+
+def squarocol?(matrix)
+    matrix.each { |array| return true if check_row(array) }
+    matrix[0].each.with_index do |ele, i|
+        return true if (1...matrix.length).all? { |row| ele == matrix[row][i] }
+    end
+    false
+end
+
+def check_row(array)
+    array.all?(array[0])
+end
+
+
+# p squarocol?([
+#     [:o, :x , :o],
+#     [:o, :x , :o],
+#     [:o, :x , :o],
+# ]) # true
+# p squarocol?([
+#     [:o, :o, :o],
+#     [:o, :o, :o],
+#     [:x, :x, :x],
+# ]) # true
+# p squarocol?([
+#     [:o, :x , :o],
+#     [:x, :o , :x],
+#     [:o, :x , :o],
+# ]) # false
+# p squarocol?([
+#     [1, 2, 2, 7],
+#     [1, 6, 6, 7],
+#     [0, 5, 2, 7],
+#     [4, 2, 9, 7],
+# ]) # true
+# p squarocol?([
+#     [1, 2, 2, 7],
+#     [1, 6, 6, 0],
+#     [0, 5, 2, 7],
+#     [4, 2, 9, 7],
+# ]) # false
+
+def pascals_triangle()
+
+end
+
+# p pascals_triangle(5)
+# [
+#     [1],
+#     [1, 1],
+#     [1, 2, 1],
+#     [1, 3, 3, 1],
+#     [1, 4, 6, 4, 1]
+# ]
+#
+# p pascals_triangle(7)
+# [
+#     [1],
+#     [1, 1],
+#     [1, 2, 1],
+#     [1, 3, 3, 1],
+#     [1, 4, 6, 4, 1],
+#     [1, 5, 10, 10, 5, 1],
+#     [1, 6, 15, 20, 15, 6, 1]
+# ]
+
+def mersenne_prime()
+
+end
+
+# p mersenne_prime(1) # 3
+# p mersenne_prime(2) # 7
+# p mersenne_prime(3) # 31
+# p mersenne_prime(4) # 127
+# p mersenne_prime(6) # 131071
+
+def triangular_word?()
+
+end
+
+# p triangular_word?('abc')       # true
+# p triangular_word?('ba')        # true
+# p triangular_word?('lovely')    # true
+# p triangular_word?('question')  # true
+# p triangular_word?('aa')        # false
+# p triangular_word?('cd')        # false
+# p triangular_word?('cat')       # false
+# p triangular_word?('sink')      # false
+
+def consecutive_collapse()
+
+end
+
+# p consecutive_collapse([3, 4, 1])                     # [1]
+# p consecutive_collapse([1, 4, 3, 7])                  # [1, 7]
+# p consecutive_collapse([9, 8, 2])                     # [2]
+# p consecutive_collapse([9, 8, 4, 5, 6])               # [6]
+# p consecutive_collapse([1, 9, 8, 6, 4, 5, 7, 9, 2])   # [1, 9, 2]
+# p consecutive_collapse([3, 5, 6, 2, 1])               # [1]
+# p consecutive_collapse([5, 7, 9, 9])                  # [5, 7, 9, 9]
+# p consecutive_collapse([13, 11, 12, 12])              # []
+
+
+def next_prime()
+
+end
+
+# p pretentious_primes([4, 15, 7], 1)           # [5, 17, 11]
+# p pretentious_primes([4, 15, 7], 2)           # [7, 19, 13]
+# p pretentious_primes([12, 11, 14, 15, 7], 1)  # [13, 13, 17, 17, 11]
+# p pretentious_primes([12, 11, 14, 15, 7], 3)  # [19, 19, 23, 23, 17]
+# p pretentious_primes([4, 15, 7], -1)          # [3, 13, 5]
+# p pretentious_primes([4, 15, 7], -2)          # [2, 11, 3]
+# p pretentious_primes([2, 11, 21], -1)         # [nil, 7, 19]
+# p pretentious_primes([32, 5, 11], -3)         # [23, nil, 3]
+# p pretentious_primes([32, 5, 11], -4)         # [19, nil, 2]
+# p pretentious_primes([32, 5, 11], -5)         # [17, nil, nil]
